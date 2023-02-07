e5ccec1b073649d982611d0112798cf4491cb46c
MartinMurithi <wachiramartin442@gmail.com>
2023-02-07 09:06:23 +0300

    Made changes

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react'
 
 export class Counter extends Component {
-  constructor(props){
-    super(props);
-    this.state = {count : 0};
-    this.handleIncreament = this.handleIncreament.bind(this);
-    this.handleDecreament = this.handleDecreament.bind(this);
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.state = {count : 0};
+  //   this.handleIncreament = this.handleIncreament.bind(this);
+  //   this.handleDecreament = this.handleDecreament.bind(this);
+  // }
 
-  handleIncreament(){
-    this.setState({count:1});
-  }
+  // handleIncreament(){
+  //   this.setState({count:1});
+  //   this.setState(prev=>{
+  //     return{count :prev.count -1};
+  //   })
+  // }
 
-  handleDecreament(){
-    //this.setState({count:10});
-    this.setState(prev=>{
-      return {count :prev.count + 1}
-     })
-  }
+  // handleDecreament(){
+  //   //this.setState({count:10});
+  //   this.setState(prev=>{
+  //     return {count:prev.count + 1}
+  //    })
+  // }
 
-  render() {
-    return (
-      <div className='counter'>
-        <p className='value'>{this.state.count}</p>
-        <button onClick={this.handleIncreament}>+</button>
-        <button onClick={this.handleDecreamnent}>-</button>
-
-      </div>
-    )
-  }
+  state= {
+    count:0
+ }
+ handleIncrement=()=>{
+  this.setState({count:this.state.count+1});
+ }
+ handleDecrement=()=>{ 
+  this.setState({count:this.state.count-1});
+  
 }
 
-export default Counter
+        render() {
+        return(
+            <div className = 'counter'>
+                <p className ='numbers'>{this.state.count}</p>
+                <span className ='button'>
+                    <button onClick={this.handleIncrement}> + </button>
+                    <button onClick={this.handleDecrement} > - </button>
+                </span>
+
+            </div>
+        )
+        }
+      }
+
+      export default Counter;
